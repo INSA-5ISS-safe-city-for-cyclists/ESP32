4821a5ec46cb9de80b4869bd11a4c293472460ae
T-berton <tberton@etud.insa-toulouse.fr>
2021-11-25 11:25:38 +0100

    Init

---
+++ src/ble.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+
+void setup() {
+    // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
+  Serial.begin(9600);
+
+
+
+
+}
+
+
+void loop() {
+
+}
